b5a053949102ad1c5a6848eda5c6ff177c87fb0d
Mohamed Fehmi <mohamed.fehmi@actiworks.de>
2019-12-15 12:59:42 +0100

    Place order with datetime :utc now: instead of :now:

--- PastryShop/Models/Repositories/OrderRepository.cs
+++ PastryShop/Models/Repositories/OrderRepository.cs
@@ -17,7 +17,7 @@ namespace PastryShop.Models.Repositories
 
         public void CreateOrder(Order order)
         {
-            order.OrderPlaced = DateTime.Now;
+            order.OrderPlaced = DateTime.UtcNow;
 
             var shoppingCardItems = _shoppingCard.ShoppingCardItems;
             order.OrderTotal = _shoppingCard.GetShoppingCardTotal();
