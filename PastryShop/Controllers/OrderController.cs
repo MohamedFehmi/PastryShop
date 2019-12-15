1bf59eefca5d1e243275254afc406ac2f7c68d19
Mohamed Fehmi <mohamed.fehmi@actiworks.de>
2019-12-15 13:20:30 +0100

    Added checkout form

---
+++ PastryShop/Controllers/OrderController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PastryShop.Models;
+using PastryShop.Models.Interfaces;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PastryShop.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly ShoppingCard _shoppingCard;
+
+        public OrderController(IOrderRepository orderRepository, ShoppingCard shoppingCard)
+        {
+            _orderRepository = orderRepository;
+            _shoppingCard = shoppingCard;
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+    }
+}
